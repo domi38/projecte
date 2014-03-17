edf5442a532807eaaa4d79a07b9b34e364c15806
alex_dominguez <alexdominguez38@msn.com>
2014-03-17 13:03:51 +0100

    created game folder+gamecontroller+reversi moved to Game folder

---
+++ Projecte/Controllers/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projecte.Controllers
+{
+    using System.Web.Mvc;
+    using Data.Entities;
+    using Data.Services;
+    using Models;
+    using MongoDB.Bson;
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            var gameService = new GameService();
+            var gameDetails = gameService.GetGamesDetails(100, 0);
+            return View(gameDetails);
+        }
+        public ActionResult Reversi()
+        {
+            return View(new Player());
+        }
+
+    }
+}
